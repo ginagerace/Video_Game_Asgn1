448226645b2ce1a426a07441ab4940ee41ff06c1
ginagerace <ggmari99@gmail.com>
2020-01-28 22:44:12 -0600

    Add files via upload

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Score scoreManager;
+    public int value;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        scoreManager.AddPoint(value);
+
+        Debug.Log("Collision Detected");
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
